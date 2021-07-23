59e9379ad4800926d090b0d42dc7072dc89193b9
Tan Ye Han <tonyhan0309@gmail.com>
2021-07-23 09:30:48 +0800

    initializing asgm

---
+++ src/dsa_assignment/DSA_Assignment.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dsa_assignment;
+
+/**
+ *
+ * @author tonyh
+ */
+public class DSA_Assignment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
+//https://docs.oracle.com/javase/8/docs/technotes/guides/collections/overview.html
